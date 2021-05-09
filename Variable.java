cd7bb5c1db8a4f2b147a55089f8e895f73a33501
pts1006 <76603521+pts1006@users.noreply.github.com>
2021-05-09 09:11:46 +0900

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,18 @@
+
+public class Variable {
+
+	public static void main(String[] args) {
+		
+		// a = 1; // Number ~> integer(정수) ... -2, -1, 0, 1, 2 등등
+		int a = 1;
+		System.out.println(a);
+		
+		//int b = 1.1; // 틀린 표현임. 1.1은 real nember(실수)... -2.0, -1.0, 0, 1.0, 2.0 등등
+		double b = 1.1;
+		System.out.println(b);
+		
+		String c = "Hello World";
+		System.out.println(c);
+	}
+		// 자바에서는 변수를 만들 때 그 변수가 어떤 데이터 타입을 담을 수 있는지를 명확하게 표현해 주어야 한다.
+}
