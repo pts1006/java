537f42aee720f653b26e20fb1da4006486ca97d3
pts1006 <76603521+pts1006@users.noreply.github.com>
2021-05-12 19:25:32 +0900

    Add files via upload

---
+++ DataType.java
@@ -0,0 +1,16 @@
+public class DataType {
+	public static void main(String[] args) {
+		/*
+		system.out.print 시리지는 개발자가 디버깅을 하기 위해서 출력으로 하는것이고.
+		세세하게 데이터랑 어떤 형태 전환이 잘 일어났나 보려고 ㅇㅇ..
+		console.log 라고 생각하면 된다.
+		
+		ln => 엔터라고 생각하면 됨.
+		*/
+		System.out.print(0+"\n");
+		
+		System.out.println(6);
+		
+		System.out.print("ㅇ");
+	}
+}
